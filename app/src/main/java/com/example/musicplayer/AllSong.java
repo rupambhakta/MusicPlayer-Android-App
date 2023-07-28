d517db4c06c23cb3c5d0904d16faba86af0629be
Rupam <rupambhakta2020@gmail.com>
2023-07-29 02:02:42 +0530

    ...

---
+++ app/src/main/java/com/example/musicplayer/AllSong.java
@@ -0,0 +1,14 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllSong extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_song);
+    }
+}
